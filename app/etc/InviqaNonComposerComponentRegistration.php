3bb852b536241086d7c9100f9ff5755b11dad882
tkotosz <tkotosz@inviqa.com>
2019-11-26 15:56:00 +0100

    Initial project setup

---
+++ app/etc/InviqaNonComposerComponentRegistration.php
@@ -0,0 +1,16 @@
+<?php
+
+$pathList[] = realpath(__DIR__ . '/../..') . '/src/*/*/cli_commands.php';
+$pathList[] = realpath(__DIR__ . '/../..') . '/src/*/*/registration.php';
+$pathList[] = realpath(__DIR__ . '/../../') . '/vendor/inviqa/toom-seo-suite/packages/*/*/registration.php';
+
+foreach ($pathList as $path) {
+    // Sorting is disabled intentionally for performance improvement
+    $files = glob($path, GLOB_NOSORT);
+    if ($files === false) {
+        throw new \RuntimeException('glob() returned error while searching in \'' . $path . '\'');
+    }
+    foreach ($files as $file) {
+        include $file;
+    }
+}
