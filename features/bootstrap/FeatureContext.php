9e6cbe101b2f1478cf4d966b5676b23920c3e84d
tkotosz <tkotosz@inviqa.com>
2019-12-01 15:57:02 +0100

    add some example to test scrutinizer config

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,22 +1,68 @@
 <?php
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
+use Inviqa\HelloWorld\Service\HelloService;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
+    /** @var HelloService */
+    private $helloWorldService;
+
+    /** @var string|null */
+    private $userName = null;
+
+    /** @var string|null */
+    private $message = null;
+
     public function __construct()
     {
+        $this->helloWorldService = new HelloService();
+    }
+
+    /**
+     * @Given the user is unknown
+     */
+    public function theUserIsUnknown()
+    {
+        $this->userName = null;
+    }
+
+    /**
+     * @Given the user is called :name
+     */
+    public function theUserIsCalled(string $name)
+    {
+        $this->userName = $name;
+    }
+
+    /**
+     * @When the user runs the hello world service
+     */
+    public function theUserRunsTheHelloWorldService()
+    {
+        if ($this->userName === null) {
+            $this->message = $this->helloWorldService->sayHello();
+        } else {
+            $this->message = $this->helloWorldService->sayHelloToSomeone($this->userName);
+        }
+    }
+
+    /**
+     * @Then a general hello message is shown
+     */
+    public function aGeneralHelloMessageIsShown()
+    {
+        assert("Hello World!" === $this->message);
+    }
+
+    /**
+     * @Then a hello message for :name is shown
+     */
+    public function aHelloMessageForIsShown(string $name)
+    {
+        assert(sprintf("Hello %s!", $name) === $this->message);
     }
 }
--- src/Inviqa/HelloWorld/Service/HelloService.php
+++ src/Inviqa/HelloWorld/Service/HelloService.php
@@ -1,8 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Inviqa\HelloWorld\Service;
 
-class HelloService
+final class HelloService
 {
     public function sayHello(): string
     {
